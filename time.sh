febe19761921c880dc8ff2e3986d0be2931239d4
Your Name <you@example.com>
2019-07-25 13:03:05 +0800

    tianjia

--- time.sh
+++ time.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for a in `seq 0 2`
 do
-   for b in `seq 0 3`
+   if [ $a -le 1 ];then
+   for b in `seq 0 9`
    do 
      for c in `seq 0 5 `
      do 
@@ -11,7 +12,7 @@ do
            do
               for f in `seq 0 9`
               do
-                echo -e "$f$e:$d$c:$b$a""\b\c"
+                echo -e "$a$b:$c$d:$e$f""\b\c"
                 sleep 1
                 #clear
                 echo -e "\b\b\b\b\b\b\b\b\c"
@@ -20,4 +21,24 @@ do
         done
      done
    done
+  else 
+   for b in `seq 0 3`
+   do
+     for c in `seq 0 5 `
+     do
+        for d in `seq 0 9`
+        do
+           for e in `seq 0 5`
+           do
+              for f in `seq 0 9`
+              do
+                echo -e "$a$b:$c$d:$e$f"
+                sleep 1
+                clear
+              done
+           done
+        done
+     done
+   done
+   fi
 done
